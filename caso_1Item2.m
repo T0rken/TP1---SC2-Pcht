e6a75331e077ed87d7f0d73694636591eea811b8
T0rken <68304807+T0rken@users.noreply.github.com>
2024-04-16 08:38:37 -0300

    Update and rename caso_1Item2 to caso_1Item2.m

---
+++ caso_1Item2.m
@@ -0,0 +1,28 @@
+% Caso 1 - item 2
+
+clear all; clc; close all
+
+%genero una matriz con los datos proporcionados
+Data = readmatrix('G:\My Drive\a Ingenieria\2024 Primer Semestre\Sistemas de Control 2\Entregas\Pucheta\Act N1 - fecha max 23_04\Consignas\Curvas_Medidas_RLC_2024.xls');
+DataBis = readmatrix('Curvas_Medidas_RLC_2024.xls');
+ 
+fig = figure(1);
+fig.Name = 'Corriente';
+plot(Data(:,1),Data(:,2)); %genero un grafico de la corriente (col 2) en funcion del tiempo (col 1)
+title('Corriente i') %esta es una variable de estado
+xlabel('Tiempo [s]');
+ylabel('Corriente [A]');
+
+fig = figure(2);
+fig.Name = 'Tension del Capacitor';
+plot(Data(:,1),Data(:,3)); %genero un grafico de la tension del capacitor (col 3) en funcion del tiempo (col 1)
+title('Tension Vc') %esta es una variable de estado
+xlabel('Tiempo [s]');
+ylabel('Tension [V]');
+
+fig = figure(3);
+fig.Name = 'Tension de Entrada';
+plot(Data(:,1),Data(:,4)); %genero un grafico de la tension de entrada (col 4) en funcion del tiempo (col 1)
+title('Tension Vin') %esta es una variable de entrada
+xlabel('Tiempo [s]');
+ylabel('Tension [V]');
