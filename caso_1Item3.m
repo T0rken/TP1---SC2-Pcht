aa7f7803a61af85228e77aee390232d07a205870
T0rken <68304807+T0rken@users.noreply.github.com>
2024-04-20 12:24:26 -0300

    Update caso_1Item3.m
    desarrollo item 2
    - Desarrollo analitico
    - Graficos de respuesta ante entrada TL como rampa

--- caso_1Item3.m
+++ caso_1Item3.m
@@ -91,3 +91,82 @@ title('Posicion angular tita_t');
 xlabel('Tiempo [s]');
 ylabel('Posicion [rad]');
 xlim([0,2]);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Caso 2 - item 1
+%    "Obtener el torque máximo que puede soportar el motor modelado mediante las Ecs. (1-5) 
+%   (1-6) y (1-7) cuando se lo alimenta con 12V, graficando para 5 segundos de tiempo la velocidad 
+%   angular y corriente ia para establecer su valor máximo como para dimensionar dispositivos 
+%   electrónicos."
+
+%   Analiticamente, puede observarse que para las condiciones en que se
+%   tiene TLmax, la ecuacion que permite hallarlo es:
+%               TLmax = Ki * iAmax
+%   De manera que conociendo el valor maximo de la corriente de armadura,
+%   se puede obtener el valor de el torque maximo que puede soportar el
+%   motor en cuestion
+
+%   Obtengo el valor maximo de iA
+iAmax = max(x1);
+
+%   Obtengo el valor maximo de TL
+TLmax = Ki*iAmax        % TLmax = 0.0021 [N.m]
+
+%   Otra forma de analizarlo podria ser mediante el uso de una rampa para
+%   el valor de entrada TL, y observando en las graficas el valor de TL al
+%   cual wr es nula, e iA es maxima
+
+%   Defino la señal de entrada T_L -> una rampa que inicia a partir del t=1[s] 
+u2(1,1) = 0;
+q = 0;
+for i=(round(1/delta_t)+1):1:(puntos_sim)
+    u2(1,i) = u2(1,i-1)+0.000000006;
+    %u2(1,i) = 0.0010;
+end
+%   defino el vector de entrada (en realidad es una matriz)
+u = [u1;u2];
+%supongo condiciones iniciales de las variables de estado  nulas
+x(1,1) = 0;
+x(2,1) = 1800;
+x(3,1) = 0;
+
+for ii=1:1:(puntos_sim+1)
+    if ii>=(round(1/delta_t))
+    x1(1,ii)= x(1,1);
+    x2(1,ii)= x(2,1);
+    x3(1,ii)= x(3,1);
+    end
+    xp = matA*x+matB*u(:,i);
+    x = x+delta_t*xp;   %la matriz X se va modificando con cada iteracion
+end
+fig = figure(5);
+fig.Name = 'Entrada TL';
+plot(t,u2);
+title('Entrada TL');
+
+%   Puedo ahora graficar las variables de interes
+%   Corriente iA
+fig = figure(6);
+fig.Name = 'Salida iA';
+plot(t,x1);
+title('Corriente de i_A');
+xlabel('Tiempo [s]');
+ylabel('Corriente [A]');
+
+%   Velocidad angular wr
+fig = figure(7);
+fig.Name = 'Salida wr';
+plot(t,x2);
+title('Velocidad angular wr');
+xlabel('Tiempo [s]');
+ylabel('Velocidad angular [rad/s]');
+
+%   Posicion tita_t
+fig = figure(8);
+fig.Name = 'Posicion angular tita_t';
+plot(t,x3);
+title('Posicion angular tita_t');
+xlabel('Tiempo [s]');
+ylabel('Posicion [rad]');
+
+
