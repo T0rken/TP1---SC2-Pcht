a4e87a1fc1ba4abb3b94cc69978db1debc7cbc7d
T0rken <68304807+T0rken@users.noreply.github.com>
2024-04-21 19:45:58 -0300

    Create caso_2Item6.m

---
+++ caso_2Item6.m
@@ -0,0 +1,8 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Caso 2 - item 6
+clear all; clc; close all
+%
+%   "Implementar un PID en tiempo discreto para que el ángulo del motor 
+%   permanezca en una referencia de 1radian sometido al torque descripto en
+%   la Fig. 1-3. (Tip: partir de KP=0,1; Ki=0,01; KD=5)."
+%
